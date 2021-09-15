5d571b138f58669634c5aedc9ab066b660b56e0b
tsvetislav <tsvetislavt99@gmail.com>
2021-09-15 10:23:51 +0300

    done with routing

--- server.js
+++ server.js
@@ -1,4 +1,50 @@
-import http from "http";
+import http from 'http';
+import fs from 'fs';
 const port = process.argv[2];
 
-console.log(port);
+http
+  .createServer((req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    switch (req.url) {
+      case '/':
+        res.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
+        let indexPage = fs.readFileSync('./views/home/index.html');
+        console.log('finished loading index.html...');
+        res.write(indexPage);
+        res.end();
+        break;
+      case '/content/styles/site.css':
+        res.writeHead(200, 'OK', { 'Content-type': 'text/css' });
+        console.log('Reading CSS...');
+        let cssFile = fs.readFileSync('./content/styles/site.css');
+        res.write(cssFile);
+        res.end();
+        break;
+      case '/content/images/pawprint.ico':
+        res.writeHead(200, 'OK', {
+          'Content-type': 'image/vnd.microsoft.icon',
+        });
+        let iconFile = fs.readFileSync('./content/images/pawprint.ico');
+        res.write(iconFile);
+        res.end();
+        break;
+      case '/add-breed':
+        res.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
+        let addBreedPage = fs.readFileSync('./views/addBreed.html');
+        res.write(addBreedPage);
+        res.end();
+        break;
+      case '/add-cat':
+        res.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
+        let addCatPage = fs.readFileSync('./views/addCat.html');
+        res.write(addCatPage);
+        res.end();
+        break;
+      default:
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.info(`HTTP Server listening on port ${port}....`);
+  });
