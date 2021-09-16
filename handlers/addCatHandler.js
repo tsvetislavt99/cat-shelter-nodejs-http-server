00a61e9d639f64773c177582f7ad6b278321f5df
tsvetislav <tsvetislavt99@gmail.com>
2021-09-16 09:41:11 +0300

    added functionality to delete garbage files that come from formidableon each photo upload

--- handlers/addCatHandler.js
+++ handlers/addCatHandler.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { exec } from 'child_process';
 import formidable from 'formidable';
 
 export default function addCatHandler(req, res) {
@@ -22,12 +23,21 @@ export default function addCatHandler(req, res) {
     const uploadFolder = path.join(__dirname, 'content', 'images');
     form.uploadDir = uploadFolder;
     form.parse(req, (err, fields, files) => {
+      if (err) throw err;
+
       let oldPath = files.upload.path;
       let newPath =
         path.join(__dirname, 'content', 'images') + '/' + files.upload.name;
       let rawData = fs.readFileSync(oldPath);
       fs.writeFileSync(newPath, rawData);
-
+      //This works only for UNIX OS and deletes the garbage files after formidable uploads.
+      exec('rm -r ./content/images/upload_*', (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+      });
+      //Please remove the exec command if you are not on a UNIX OS
       fs.readFile('./db/cats.json', 'utf-8', (err, data) => {
         if (err) throw err;
 
